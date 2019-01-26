d23c51ba5feccd338dc2fc73e63c2abd6476b505
darshan3899 <46934281+darshan3899@users.noreply.github.com>
2019-01-26 22:10:39 +0530

    Add files via upload

---
+++ Upper_lower_case.py
@@ -0,0 +1,5 @@
+#WAP to conver a string to upper case & lower case
+#!usr/bin/python
+s=str(input())
+print(s.upper())
+print(s.lower())
---
+++ string_startswith.py
@@ -0,0 +1,4 @@
+#WAP to check if string starts with T
+#!usr/bin/python
+s=str(input("Enter a String"))
+print(s.startswith("T"))
