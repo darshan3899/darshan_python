0a3685667f406ce11fed74972d8a16f9cff8e2b0
darshan3899 <46934281+darshan3899@users.noreply.github.com>
2019-01-27 16:44:20 +0530

    Update if_elsee.py

--- if_elsee.py
+++ if_elsee.py
@@ -1,5 +1,5 @@
 #WAP to accept no of donuts.If no of donuts is greater than 10 print-No of Donuts is many....Else print-No of donuts is (count)
-#!usr/bin/python
+#!/usr/bin/python
 a=int(input("Enter Number of Donuts"))
 if a>10:
 	print("Number of Donuts:MANY")
