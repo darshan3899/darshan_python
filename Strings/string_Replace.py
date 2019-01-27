d2457edfa3f96de7b4aa565660c53d28d6ef359a
darshan3899 <46934281+darshan3899@users.noreply.github.com>
2019-01-27 16:54:24 +0530

    Add files via upload

---
+++ Strings/string_Replace.py
@@ -0,0 +1,4 @@
+#WAP to accept a string from user and repalce the occurance of first letter with *
+#!/usr/bin/python
+s=str(input())
+print(s[0]+s[1:].replace(s[0],"*"))
---
+++ Strings/string_startswith_fn.py
@@ -0,0 +1,7 @@
+#WAP to check if string starts with T
+#!/usr/bin/python
+def start(s):
+	print(s.startswith("T"))
+	
+s=str(input("Enter a String"))
+start(s)
