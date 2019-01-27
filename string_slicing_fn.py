ae58fe094c454493a82e82255e1754d26f47ebc3
darshan3899 <46934281+darshan3899@users.noreply.github.com>
2019-01-27 16:49:41 +0530

    Delete string_slicing_fn.py

--- string_slicing_fn.py
+++
@@ -1,7 +0,0 @@
-#WAP to accept strin gfrom user and return a string having first 2 and last 2 characters of i/p string
-#!/usr/bin/python
-def slice(s):
-	s=s[:2]+s[-2:]
-	return s
-s=str(input("Enter a String"))
-print(slice(s))
