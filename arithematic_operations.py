7d41aaf7f4b48b4fe7170ed4a7612202d15279ad
darshan3899 <46934281+darshan3899@users.noreply.github.com>
2019-01-27 16:45:26 +0530

    Delete arithematic_operations.py

--- arithematic_operations.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python 
-
-x=int(input("Enter Number 1 "))
-y=int(input("Enter Number 2 "))
-print("ADDITION=",x+y)
-print("SUBTRACTION=",x-y)
-print("MULTIPLICATION=",x*y)
-print("DIVISION=",x/y)
-print("EXPONENT=",x**y)
