a33fa246d40bf25a92f63f63fcef5dddbe37bcc6
darshan3899 <46934281+darshan3899@users.noreply.github.com>
2019-02-03 15:44:28 +0530

    Delete if_elsee.py

--- Basics/if_elsee.py
+++
@@ -1,7 +0,0 @@
-#WAP to accept no of donuts.If no of donuts is greater than 10 print-No of Donuts is many....Else print-No of donuts is (count)
-#!/usr/bin/python
-a=int(input("Enter Number of Donuts"))
-if a>10:
-	print("Number of Donuts:MANY")
-else:
-	print("Number of Donuts:",a)
