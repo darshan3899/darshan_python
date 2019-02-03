a5f2de1ec33e7085d695a46b0f4a43a1e80859ad
darshan3899 <46934281+darshan3899@users.noreply.github.com>
2019-02-03 15:46:01 +0530

    Delete string_rotation.py

--- Strings/string_rotation.py
+++
@@ -1,9 +0,0 @@
-#WAP to Check if string is rotation of another
-#!/usr/bin/python
-s=str(input("Enter The String"))
-s1=str(input("Enter Rotation to Check"))
-if len(s1)==len(s):
-	print(s1 in s+s)
-else:
-	print("False")
-
